5a20843b613d11e10f0a8d83178004748aec2e01
anaelChardan <anael.chardan@gmail.com>
2016-12-27 15:22:56 +0100

    Add the Asset and launcher

--- AkeneoMacApp/AppDelegate.swift
+++ AkeneoMacApp/AppDelegate.swift
@@ -11,17 +11,21 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "StatusBarButtonImage")
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        statusItem.menu = statusMenu
     }
-
-
 }
 
