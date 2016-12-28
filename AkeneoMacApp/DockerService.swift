e568fb3ba969333aece8aff66e82695b3269165e
anaelChardan <anael.chardan@gmail.com>
2016-12-28 14:58:39 +0100

    Interact with Docker vol1

---
+++ AkeneoMacApp/DockerService.swift
@@ -0,0 +1,13 @@
+//
+//  DockerService.swift
+//  AkeneoMacApp
+//
+//  Created by Ananas-Mac on 28/12/2016.
+//  Copyright © 2016 Ananas-Mac. All rights reserved.
+//
+
+import Cocoa
+
+class DockerService: NSObject {
+
+}
